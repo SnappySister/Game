38efd5d5a72858bae6c9b93b17c8e6f2bb0dfa88
陈科嘉 <chenkejia@cmiot.chinamobile.com>
2026-07-07 17:41:00 +0800

    【四川麻将】修复bug：修复对局总账总计

--- lobby/games/sichuan-mahjong.js
+++ lobby/games/sichuan-mahjong.js
@@ -18,7 +18,6 @@ class SichuanMahjongEngine {
     this._send = sendFn;
     this._log = logFn || (() => {});
     this.playerCount = players.length;
-    this.initialScores = [...initialScores];  // 保存本局开始时的分数,用于计算变化量
     this.players = players.map((p, i) => ({
       ...p,
       index: i,
@@ -1186,11 +1185,11 @@ class SichuanMahjongEngine {
     const settlement = {
       flowEnd: isFlowEnd,
       nextDealer: this.dealerIndex,
-      players: this.players.map((p, i) => ({
+      players: this.players.map(p => ({
         name: p.name,
         score: p.score,
         isHu: p.isHu,
-        change: p.score - (this.initialScores[i] || 0),  // 本局变化量 = 当前分数 - 初始分数
+        change: p.score,
         melds: p.melds.map(m => ({ type: m.type, label: tileLabel(m.tiles[0]) })),
         fanDetail: p.fanDetail || null,
       })),
